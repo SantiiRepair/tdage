e2e387bb104a0b068267e6e2d5691936d61fc607
Santiago Ramirez <miguelsantiago1940@gmail.com>
2024-09-02 03:33:05 -0400

    fixed buggy results

--- tdage.go
+++ tdage.go
@@ -1,8 +1,8 @@
 package tdage
 
 import (
-	"fmt"
 	"math"
+	"sort"
 	"time"
 )
 
@@ -22,28 +22,22 @@ type Result struct {
 // minimum and maximum ID values from a dataset called `ages`.
 //
 // Returns:
-//   A pointer to a new instance of AgeData, with the minID and maxID
-//   fields set according to the IDs found in the dataset.
+//
+//	A pointer to a new instance of AgeData, with the minID and maxID
+//	fields set according to the IDs found in the dataset.
 func NewPool() *AgeData {
 	a := &AgeData{}
 
 	for k := range ages {
-		id := parseID(k)
-		if a.minID == 0 || id < a.minID {
-			a.minID = id
+		if a.minID == 0 || k < a.minID {
+			a.minID = k
 		}
-		if a.maxID == 0 || id > a.maxID {
-			a.maxID = id
+		if a.maxID == 0 || k > a.maxID {
+			a.maxID = k
 		}
 	}
 
 	return a
-}
-
-func parseID(s string) int64 {
-	var id int64
-	fmt.Sscanf(s, "%d", &id)
-	return id
 }
 
 // Calculates the creation date based on the provided integer value.
@@ -51,29 +45,38 @@ func parseID(s string) int64 {
 // It returns a tuple containing the result of the calculation and the date of creation as a time.Time object.
 //
 // Parameters:
-//   v: An integer representing the value used to determine the creation date.
+//
+//	v: An integer representing the value used to determine the creation date.
 //
 // Returns:
-//   A tuple containing the result of the calculation and the date of creation as a time.Time object.
+//
+//	A tuple containing the result of the calculation and the date of creation as a time.Time object.
 func (a *AgeData) GetAsDatetime(v int64) (int, time.Time) {
 	if v < a.minID {
-		return -1, time.Unix(ages[fmt.Sprintf("%d", a.minID)]/1000, 0)
+		return -1, time.Unix(ages[a.minID]/1000, 0)
 	} else if v > a.maxID {
-		return 1, time.Unix(ages[fmt.Sprintf("%d", a.maxID)]/1000, 0)
+		return 1, time.Unix(ages[a.maxID]/1000, 0)
 	}
 
+	keys := make([]int64, 0, len(ages))
+	for k := range ages {
+		keys = append(keys, k)
+	}
+
+	sort.Slice(keys, func(i, j int) bool {
+		return keys[i] < keys[j]
+	})
+
 	lowerID := a.minID
-	for x, y := range ages {
-		uid := parseID(x)
-		if v <= uid {
-			lage := float64(ages[fmt.Sprintf("%d", lowerID)]) / 1000
-			uage := float64(y) / 1000
-			idRatio := float64(v-lowerID) / float64(uid-lowerID)
-			midDate := math.Floor((idRatio * (uage - lage)) + lage)
+	for _, k := range keys {
+		if v <= k {
+			lage := float64(ages[lowerID]) / 1000
+			uage := float64(ages[k]) / 1000
+			vRatio := float64(v-lowerID) / float64(k-lowerID)
+			midDate := math.Floor((vRatio * (uage - lage)) + lage)
 			return 0, time.Unix(int64(midDate), 0)
 		}
-
-		lowerID = uid
+		lowerID = k
 	}
 
 	return 0, time.Time{}
@@ -84,11 +87,13 @@ func (a *AgeData) GetAsDatetime(v int64) (int, time.Time) {
 // It returns a Result containing the status of the date comparison and the corresponding date.
 //
 // Parameters:
-//   v: An integer representing the value used to determine the creation date.
+//
+//	v: An integer representing the value used to determine the creation date.
 //
 // Returns:
-//   A Result struct containing the status of the comparison ("older_than", "newer_than", or "aprox")
-//   and the date as a time.Time object, along with an error if applicable.
+//
+//	A Result struct containing the status of the comparison ("older_than", "newer_than", or "aprox")
+//	and the date as a time.Time object, along with an error if applicable.
 func (a *AgeData) GetDate(v int64) (Result, error) {
 	s, d := a.GetAsDatetime(v)
 	var status string
